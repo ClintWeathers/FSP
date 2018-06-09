815454d86e321b741bfe145048cfa03a2bf07046
Clint Weathers <clintweathers@gmail.com>
2018-06-08 23:47:16 -0500

    lets see if this is more elegant (and works)

---
+++ newblah.py
@@ -0,0 +1,17 @@
+from flask import *
+import pandas as pd
+app = Flask(__name__)
+
+@app.route("/tables")
+def show_tables():
+    data = pd.read_csv('iris.csv')
+    #data.set_index(['Name'], inplace=True)
+    data.index.name=None
+    setosa = df.loc[df.species=='setosa']
+    versicolor = df.loc[df.species=='versicolor']
+    virginica = df.loc[df.species=='virginica']
+    return render_template('view.html',tables=[setosa.to_html(classes='setosa'), versicolor.to_html(classes='versicolor'), virginica.to_html(classes='virginica')],
+    titles = ['Setosa', 'Versicolor', 'Virginica'])
+
+if __name__ == "__main__":
+    app.run(debug=True)
